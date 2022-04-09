39115df16901cea3ff73c7298a90c3ec4fd20c09
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-08 22:33:29 -0300

    first commit

---
+++ src/test/java/com/java/dojo/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dojo.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
