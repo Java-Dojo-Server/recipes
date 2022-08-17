c2c4eeefb0b52eb1658225427d2a7286a1fc9354
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-08-17 15:32:04 -0300

    Classes arrumadas

--- src/main/java/com/java/dojo/recipes/entities/Recipe.java
+++ src/main/java/com/java/dojo/recipes/entities/Recipe.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,6 +21,8 @@ import lombok.Setter;
 @Builder
 public class Recipe {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private List<String> ingredients = new ArrayList<>();
 	private List<String> preparingMode = new ArrayList<>();
 	
